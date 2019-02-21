f255d0cd0c1736ebd88aacba1a53227158d678a8
Taylor Lindsay <tgracelindsay13@gmail.com>
2019-02-21 12:11:57 +0100

    One test should fail

--- TaylorsToDos/ViewController/ViewController.swift
+++ TaylorsToDos/ViewController/ViewController.swift
@@ -1,5 +1,5 @@
 //
-//  CreateViewController.swift
+//  ViewController.swift
 //  TaylorsToDos
 //
 //  Created by Taylor Lindsay on 2/21/19.
@@ -8,28 +8,15 @@
 
 import UIKit
 
-class CreateViewController: UIViewController {
+class ViewController: UIViewController {
 
-    @IBOutlet weak var titleTextField: UITextField!
-    @IBOutlet weak var importantSwitch: UISwitch!
-    var viewModel: CreateViewModel = CreateViewModel()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func addTapped(_ sender: UIButton) {
-        let newTitle = titleTextField.text
-        let newImportance = importantSwitch.isOn
-        viewModel.createToDo(name: newTitle ?? "New", markAsImportant: newImportance)
-    }
 
-    
-    
-    
-    
     /*
     // MARK: - Navigation
 
---
+++ TaylorsToDosTests/TaylorsToDosTests.swift
@@ -0,0 +1,34 @@
+//
+//  TaylorsToDosTests.swift
+//  TaylorsToDosTests
+//
+//  Created by Taylor Lindsay on 2/21/19.
+//  Copyright © 2019 Taylor Lindsay at Babbel. All rights reserved.
+//
+
+import XCTest
+
+class TaylorsToDosTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(false)
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
