c1be4cf513ab15b05ca2687610d312d3ed0dd697
Taylor Lindsay <tgracelindsay13@gmail.com>
2019-02-21 12:34:29 +0100

    Test should now pass

--- TaylorsToDosTests/TaylorsToDosTests.swift
+++ TaylorsToDosTests/TaylorsToDosTests.swift
@@ -21,7 +21,7 @@ class TaylorsToDosTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(false)
+        XCTAssert(true)
     }
 
     func testPerformanceExample() {
