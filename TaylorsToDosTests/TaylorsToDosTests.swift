91adf5455a5ba2788353532b5e349a66807fcdd3
Taylor Lindsay <tgracelindsay13@gmail.com>
2019-02-21 15:28:13 +0100

    Take 2, tests should now pass

--- TaylorsToDosTests/TaylorsToDosTests.swift
+++ TaylorsToDosTests/TaylorsToDosTests.swift
@@ -24,11 +24,11 @@ class TaylorsToDosTests: XCTestCase {
         XCTAssert(true)
     }
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+//        self.measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
